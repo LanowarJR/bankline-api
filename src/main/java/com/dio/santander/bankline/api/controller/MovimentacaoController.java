740bb42c79ebefd00686f26826728f6847cbc5e1
Mauro Paulino de Paiva JR <mauro.paulino@gmail.com>
2022-05-15 21:13:45 -0300

    criado WebConfig.java para integrar o projeto front-end Bankline App

--- src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
@@ -1,23 +1,13 @@
 package com.dio.santander.bankline.api.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.dio.santander.bankline.api.dto.NovaMovimentacao;
-import com.dio.santander.bankline.api.dto.NovoCorrentista;
-import com.dio.santander.bankline.api.model.Correntista;
 import com.dio.santander.bankline.api.model.Movimentacao;
-import com.dio.santander.bankline.api.repository.CorrentistaRepository;
 import com.dio.santander.bankline.api.repository.MovimentacaoRepository;
-import com.dio.santander.bankline.api.service.CorrentistaService;
 import com.dio.santander.bankline.api.service.MovimentacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movimentacoes")
@@ -40,4 +30,6 @@ public class MovimentacaoController {
     public void save(@RequestBody NovaMovimentacao movimentacao) {
         service.save(movimentacao);
     }
+
+
 }
